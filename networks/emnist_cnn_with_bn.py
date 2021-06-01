671cba13e6703b444915520df3fa30ccba9769c5
ivan.morzhov <ivan.morzhov@fogstream.ru>
2021-06-01 11:48:20 +1000

    Add new architecture and loader, corrected dataset_check.py

---
+++ networks/emnist_cnn_with_bn.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+# Объявим класс для нашей нейронной сети
+class SimpleEmnistConvNet(nn.Module):
+    def __init__(self, device):
+        super(SimpleEmnistConvNet, self).__init__()
+        self.device = device
+        # определяем слои нейросети
+        self.Conv1 = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.2))
+        self.Conv2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.2))
+        self.Conv3 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.2))
+        self.fc1 = nn.Sequential(
+            nn.Linear(3 * 3 * 64, 200),
+            nn.Dropout(0.5))
+        self.fc2 = nn.Linear(200, 62)
+
+    def forward(self, x):
+        x = self.Conv1(x).to(self.device)
+        x = self.Conv2(x).to(self.device)
+        x = self.Conv3(x).to(self.device)
+        x = x.reshape(x.size(0), -1)
+        x = F.relu(self.fc1(x)).to(self.device)
+        x = self.fc2(x).to(self.device)
+        # x = self.fc3(x).to(self.device)
+        return F.log_softmax(x).to(self.device)
+
+    # вывод архитектуры нейросети
+    def __str__(self):
+        print(self)
