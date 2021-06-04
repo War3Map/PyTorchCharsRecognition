06f26a144291ca10d20b18245d068fb2e8715f65
ivan.morzhov <ivan.morzhov@fogstream.ru>
2021-06-04 11:01:13 +1000

    Added SRCNN loader code

--- datasets/srcnn_loader.py
+++ datasets/srcnn_loader.py
@@ -1,46 +1,39 @@
 from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
 
 from datasets.base_loader import BaseLoader
+from datasets.srcnn_emnist import SrcnnEmnist
 
 
-# class MnistLoader(BaseLoader):
-#     """
-#     Loader for MNIST dataset.
-#     """
-#     def __init__(self, batch_size, data_path='../dataMNIST'):
-#         """
-#         :param batch_size: batch size used in loader
-#
-#         :param data_path: path to load dataset
-#
-#
-#         """
-#         super().__init__(name="MNIST",
-#                          data_path=data_path,
-#                          batch_size=batch_size,
-#                          need_resize=True)
-#
-#     def load(self):
-#         """
-#         Load dataset
-#         :rtype: Tuple[,]
-#         :return: train and test dataset
-#         """
-#         transformations = transforms.Compose([transforms.ToTensor(),
-#                                               transforms.Normalize((0.1307,), (0.3081,))])
-#
-#         train_loader = DataLoader(
-#             datasets.MNIST(self.data_path, train=True, download=True, transform=transformations),
-#             batch_size=self.batch_size, shuffle=True, pin_memory=True)
-#
-#         labels_loader = DataLoader(
-#             datasets.MNIST(self.data_path, train=False, download=False, transform=transformations),
-#             batch_size=self.batch_size, shuffle=True, pin_memory=True)
-#
-#         return train_loader, labels_loader
+class SrcnnLoader(BaseLoader):
+    """
+    Loader for SRCNN network with emnist.
+    """
+    def __init__(self, batch_size, train_path, test_path):
+        """
+        :param batch_size: batch size used in loader
 
 
 
 
+        """
+        super().__init__(name="SRCNN",
+                         batch_size=batch_size)
+        self.train_path = train_path
+        self.test_path = test_path
 
+    def _load(self):
+        """
+        Load dataset
+        :rtype: Tuple[,]
+        :return: train and test dataset
+        """
+
+        train_loader = DataLoader(
+            SrcnnEmnist(self.train_path, train=True),
+            batch_size=self.batch_size, shuffle=True, pin_memory=True)
+
+        test_loader = DataLoader(
+            SrcnnEmnist(self.test_path, train=False),
+            batch_size=self.batch_size, shuffle=True, pin_memory=True)
+
+        return train_loader, test_loader
