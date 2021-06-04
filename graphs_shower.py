abe4f30e8a464463439016f87c1cc53da3b9d701
ivan.morzhov <ivan.morzhov@fogstream.ru>
2021-06-04 11:04:15 +1000

    Change max and min axes values for showing graphs - now depends from losses and accuraces

--- graphs_shower.py
+++ graphs_shower.py
@@ -4,6 +4,8 @@ Created on Tue Feb  4 17:17:25 2020
 
 @author: IVAN
 """
+from math import ceil
+
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
@@ -18,6 +20,10 @@ def graphics_show_loss_acc(losses, accuracies, save_file):
 
     fig, axes = plt.subplots(2, 1)
     # график значения функции потерь на каждой эпохе
+
+    max_y_val = int(ceil(max(losses))) + 1
+    max_x_val = len(losses) + 1
+
     axes[0].plot(epochs, losses)
     axes[0].grid(which='major',
                  color='k')
@@ -25,8 +31,8 @@ def graphics_show_loss_acc(losses, accuracies, save_file):
     axes[0].grid(which='minor',
                  color='gray',
                  linestyle=':')
-    axes[0].set_ylim(0, 10)
-    axes[0].set_xlim(0, 16)
+    axes[0].set_ylim(0, max_y_val)
+    axes[0].set_xlim(0, max_x_val)
 
     axes[0].set_xlabel("Эпоха")  # ось абсцисс
     axes[0].set_ylabel("Потеря")  # ось ординат
@@ -39,6 +45,7 @@ def graphics_show_loss_acc(losses, accuracies, save_file):
     axes[0].minorticks_on()
 
     # график точности на каждой эпохе
+
     axes[1].plot(epochs, accuracies)
     axes[1].grid(which='major',
                  color='k')
@@ -47,7 +54,7 @@ def graphics_show_loss_acc(losses, accuracies, save_file):
                  color='gray',
                  linestyle=':')
     axes[1].set_ylim(0.5, 1.0)
-    axes[1].set_xlim(0, 20)
+    axes[1].set_xlim(0, max_x_val)
 
     axes[1].set_xlabel("Эпоха")  # ось абсцисс
     axes[1].set_ylabel("Точность")  # ось ординат
